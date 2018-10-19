db06c25673c1a9ace371a2bb70652600eebf0f43
Emilia Paz <emilia.paz@ku.edu>
2018-10-19 17:58:59 -0500

    [user panel] sell form working with backend, changed items table to fix bug

--- users/backEnd/soldItem.php
+++ users/backEnd/soldItem.php
@@ -1,11 +1,9 @@
-
-
 <?php
     $mysqli = new mysqli("mysql.eecs.ku.edu", "t828n219", "se4ahqu3", "t828n219");
 
     /* check connection */
-    if ($mysqli->connect_error) {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_errno);
         exit();
     }
 
@@ -13,18 +11,22 @@
     $description = $_POST["description"];
     $picture = $_POST["picture"];
 
-    // create entry
-    $entry = "INSERT INTO Project_Items(name,description) VALUES ('$item','$description')";
-
-    if ($mysqli->query($entry) === TRUE) {
-        echo "New record created successfully";
+    // Check if message is not empty
+    if (!empty($item) && !empty($description) ){
+        // Create post
+        $post = "INSERT INTO  Project_Items(name, description) VALUES ('$item','$description');";
+        if ($mysqli->query($post) === TRUE) {
+            echo "New record created successfully";
+        }
+        else {
+            echo "Error";
+        }
     }
-    else {
-        echo "Error: ";
+    else
+    {
+        echo "Imput is not valid";
     }
 
-    // Was working, but it doesn't create the entry anymore. Will review tomorrow what is happening
-    // Missing the user!!! We need to find a way to pass the username or userid based on who sends the form
 
     /* close connection */
     $mysqli->close();
