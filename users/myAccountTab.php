943cf982c68223bfe48ec254ab31fa06738390bf
Thor Lyche <thorlyche1@gmail.com>
2018-10-20 11:24:58 -0500

    Fixed format of myAccount tab lines

--- users/myAccountTab.php
+++ users/myAccountTab.php
@@ -15,7 +15,7 @@ if ( ! isset( $_SESSION['user_id'] ) ) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="cssFiles/sellTab.css">
+    <link rel="stylesheet" href="cssFiles/myAccountTab.css">
     <link rel="stylesheet" href="cssFiles/homeScreen.css">
 </head>
 <body>
