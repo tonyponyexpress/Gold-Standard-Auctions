f15532e05e5a1444ecb60b8f131a845042d26023
tonyponyexpress <tonynguyen979@ku.edu>
2018-10-19 19:08:36 -0500

    admin login works as intended

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -11,21 +11,14 @@ if ($mysqli->connect_errno) {
 
 $admin_user = $_POST["username"];
 $admin_password = $_POST["password"];
-$query = "SELECT * FROM Project_Admins WHERE username = '$admin_user';";
-$query2 = "SELECT * FROM Project_Admins WHERE password = '$admin_password';";
-$result = $mysqli->query($query);
-$result2 = $mysqli->query($query2);
 
-if (mysqli_num_rows($result) == 0{
-  echo "Error: Admin username does not exist";
+$result = mysqli_query($mysqli, "SELECT * FROM Project_Admins WHERE username='$admin_user' AND password='$admin_password';");
+
+if (mysqli_num_rows($result)){
+  header('Location: admin_dashboard.html');
 }
 else{
-  if (mysqli_num_rows($result2 == 0)){
-    echo "Error: invalid password";
-  }
-  else{
-    header('Location: admin_dashboard.html');
-  }
+  echo "Error: invalid admin username/password";
 }
 
 $mysqli->close();
