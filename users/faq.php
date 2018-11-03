324df5166b9eb8a167ec9cf3cae56f52b52a8a3e
Thor Lyche <thorlyche1@gmail.com>
2018-11-02 20:10:37 -0500

    Added accordion to faq

--- users/faq.php
+++ users/faq.php
@@ -8,6 +8,32 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- CSS -->
     <link rel="stylesheet" href="cssFiles/homeScreen.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+    .accordion {
+        background-color: #eee;
+        color: #444;
+        cursor: pointer;
+        padding: 18px;
+        width: 100%;
+        border: none;
+        text-align: left;
+        outline: none;
+        font-size: 15px;
+        transition: 0.4s;
+    }
+
+    .active, .accordion:hover {
+        background-color: #ccc;
+    }
+
+    .panel {
+        padding: 0 18px;
+        display: none;
+        background-color: white;
+        overflow: hidden;
+    }
+    </style>
 </head>
 <body>
 
@@ -35,7 +61,60 @@
     ?>
 
 
-    <h1>This will be the FAQ page, since we have none right now it is blank</h1>
+    <div class="container-fluid" >
+      <div class="row">
+        <div class="col-md-6">
+          <div class= "center" style= "text-align: center;"><h2>Frequently Asked Questions</h2></div>
+
+          <button class="accordion">Question 1</button>
+          <div class="panel">
+            <p>Answer 1</p>
+          </div>
+
+          <button class="accordion">Question 2</button>
+          <div class="panel">
+            <p>Answer 2</p>
+          </div>
+
+          <button class="accordion">Question 3</button>
+          <div class="panel">
+            <p>Answer 3</p>
+          </div>
+
+          <button class="accordion">Question 4</button>
+          <div class="panel">
+            <p>Answer 4</p>
+          </div>
+
+          <button class="accordion">Question 5</button>
+          <div class="panel">
+            <p>Answer 5</p>
+          </div>
+
+          <button class="accordion">Question 6</button>
+          <div class="panel">
+            <p>Answer 6</p>
+          </div>
+
+          <script>
+          var acc = document.getElementsByClassName("accordion");
+          var i;
+
+          for (i = 0; i < acc.length; i++) {
+              acc[i].addEventListener("click", function() {
+                  this.classList.toggle("active");
+                  var panel = this.nextElementSibling;
+                  if (panel.style.display === "block") {
+                      panel.style.display = "none";
+                  } else {
+                      panel.style.display = "block";
+                  }
+              });
+          }
+          </script>
+        </div>
+      </div>
+    </div>
 
 
     <!-- Footer -->
