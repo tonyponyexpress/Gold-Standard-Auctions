a838fd8e9a0c309eceb1652915479adf98c430f8
Ian Farris <i676f971@ku.edu>
2018-11-04 22:41:26 -0600

    added a blank template for contactUs backend

---
+++ users/backEnd/contactUs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*User CreateUsers
+*This file is in charge of creating users and storing them in a databse
+*
+*
+*
+*@author Tritens
+*@package users
+*
+*/
+
+
+// Access database
+include ('../../cms/sql_credentials.php');
+global $mysqli;
+
+
+$mysqli->close();
+
+?>
