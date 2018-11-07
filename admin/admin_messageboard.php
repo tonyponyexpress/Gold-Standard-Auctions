1f6fe1281e413317e0b41fa66f1fd98384767d33
Emilia Paz <emilia.paz@ku.edu>
2018-11-07 15:10:59 -0600

    [admin] all admin files have no footer (copied form user panel)

--- admin/admin_messageboard.php
+++ admin/admin_messageboard.php
@@ -49,7 +49,7 @@
 
         <!-- Page Content  -->
         <div id="content">
-             <h2>Users</h2>
+             <h2> Message Board</h2>
 
              <?php
                 // Access database
@@ -94,35 +94,9 @@
              /* close connection */
              $mysqli->close();
              ?>
-
-
-
         </div>
      </div>
 
-
-    <!-- Footer -->
-    <div class= "container-fluid" style="background-color: rgba(0,87,110,0.8)">
-      <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-3">
-          <br>
-          <a href="faq.html">FAQ</a><br>
-          <a href="contactUs.html">Contact Us</a><br>
-        </div>
-        <div class="col-md-3">
-          <br>
-          <a href="reportAnIssue.html">Report an Issue</a><br>
-          <a href="returnPolicy.html">Return Policy</a><br>
-        </div>
-        <div class="col-md-4" style="text-align: center">
-          <br>
-          <img src="../assets/fakeLogo.jpg" alt="Logo">
-          <br>
-          <br>
-        </div>
-      </div>
-    </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
