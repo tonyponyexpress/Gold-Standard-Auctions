0b89aed6d57a69ccfd6447b996aa49ae63228fe1
Ian Farris <i676f971@ku.edu>
2018-11-06 19:04:00 -0600

    backend works unless description contains quotes or apostrophes.

--- users/backEnd/contact.php
+++ users/backEnd/contact.php
@@ -15,7 +15,16 @@
 include ('../../cms/sql_credentials.php');
 global $mysqli;
 
+$title = $_POST["contactReason"];
+$description = $_POST["description"];
+$email = $_POST["contactEmail"];
 
+
+$add_contact = "INSERT INTO Project_Problems (title, description, email) VALUES ('$title', '$description', '$email');";
+if($user_result = $mysqli->query($add_contact)){
+  //echo "New user created successfully.<br>";
+  header('Location: ../contactUs.php');
+}
 $mysqli->close();
 
 ?>
