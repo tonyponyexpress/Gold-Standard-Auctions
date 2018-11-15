3e8a522605596ba87c27417e9dc4045fe8e5beca
Thor Lyche <thorlyche1@gmail.com>
2018-11-15 17:12:41 -0600

    Placed pages in fluid containers

--- users/contactUs.php
+++ users/contactUs.php
@@ -25,6 +25,7 @@
       $temp = new users();
       $temp->header_login_user();
   ?>
+  <div class="container-fluid">
     <div class = "row">
       <div class ="col-md-12">
         <div class = "center" style = "text-align: center; margin-top: 25px;">
@@ -35,41 +36,43 @@
     <div class = "row"></div>
     <div class="col-xs-12" style="height:100px;"></div> <!--creates blank space -->
 
-    <form class="form-contact" action="backEnd/contact.php" method="post">
-      <div class = "container-fluid">
-        <div class = "row">
-          <div class ="col-md-3"></div>
-          <div class ="col-md-6">
-            <div class = "center" style = "text-align: left;">
+      <form class="form-contact" action="backEnd/contact.php" method="post">
+        <div class = "container-fluid">
+          <div class = "row">
+            <div class ="col-md-3"></div>
+            <div class ="col-md-6">
+              <div class = "center" style = "text-align: left;">
 
-              <p> Please select your reason for contacting us:
-              <select name = "contactReason">
-                <option value = "Issue with creating an account"> Issue with creating an account</option>
-                <option value = "Problem logging in to existing account"> Problem logging in to existing account</option>
-                <option value = "Questions about the Get Rid of It process"> Questions about the Get Rid of It process</option>
-              </select>
+                <p> Please select your reason for contacting us:
+                <select name = "contactReason">
+                  <option value = "Issue with creating an account"> Issue with creating an account</option>
+                  <option value = "Problem logging in to existing account"> Problem logging in to existing account</option>
+                  <option value = "Questions about the Get Rid of It process"> Questions about the Get Rid of It process</option>
+                </select>
 
+              </p>
+              <div class="col-xs-12" style="height:25px;"></div>
+              <textarea rows="5" name = "description" cols="100" placeholder = "Please input the description of your issue here."></textarea>
+              <div class="col-xs-12" style="height:25px;"></div>
+              <p> please provide an email address we can reach you at:
+              <input type = "text" name = "contactEmail"> </input>
             </p>
-            <div class="col-xs-12" style="height:25px;"></div>
-            <textarea rows="5" name = "description" cols="100" placeholder = "Please input the description of your issue here."></textarea>
-            <div class="col-xs-12" style="height:25px;"></div>
-            <p> please provide an email address we can reach you at:
-            <input type = "text" name = "contactEmail"> </input>
-          </p>
-          </div>
+            </div>
 
-          <div class="col-xs-12" style="height:25px;"></div>
-          <div class = "col-md-3"></div>
+            <div class="col-xs-12" style="height:25px;"></div>
+            <div class = "col-md-3"></div>
+          </div>
         </div>
       </div>
-      <div class = "row">
-        <div class = "col-md-12">
-          <div class = "center" style = "text-align: center">
-            <input type = "submit" value = "Submit">
-          </div>
+        <div class = "row">
+          <div class = "col-md-12">
+            <div class = "center" style = "text-align: center">
+              <input type = "submit" value = "Submit">
+            </div>
+        </div>
       </div>
-    </div>
-  </form>
+    </form>
+  </div>
 
     <!-- Footer -->
     <?php
