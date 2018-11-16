90dffff737287e6011f31b32f9de2e933b7afc4e
Thor Lyche <thorlyche1@gmail.com>
2018-11-15 18:04:54 -0600

    Fixed all px on homescreen

--- users/homeScreen.php
+++ users/homeScreen.php
@@ -29,10 +29,10 @@
         include('templates/header_login.php');
     ?>
 
-    <div class="container-fluid">
+    <div class="container-fluid" style="margin-top: 3%;">
       <div class="row">
         <div class="col-md-2" style="text-align: center;">
-          <div class="pad" style="margin-top: 50px; margin-bottom: 100px;"><img src="../assets/guarantee.jpg" alt="Guarantee"> </div>
+          <div class="pad" style="margin-bottom: 30%;"><img src="../assets/guarantee.jpg" alt="Guarantee"> </div>
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -40,28 +40,28 @@
                     <p>"Very simple and helpful company. Provided me with free shipping material, high offer, and a quick process. Even though what I had was a used iPhone they still took the time to evaluate what I had and gave me a good offer and process to sell it."</p>
                     <footer>Ian Farris</footer>
                   </blockquote>
-                  <img src="../HeadShots/Ian.jpg" alt="Ian's headshot" style="width:60px;height:60px;">
+                  <img src="../HeadShots/Ian.jpg" alt="Ian's headshot" style="width:50%;height:50%;">
                 </div>
                 <div class="carousel-item">
                   <blockquote>
                     <p>"You can get some very impressive offers here. I would definitely recommend if you are not trying to hassle with selling things on Craigslist.  "</p>
                     <footer>Emilia Paz</footer>
                   </blockquote>
-                  <img src="../HeadShots/Emilia.jpg" alt="Emilia's headshot" style="width:60px;height:60px;">
+                  <img src="../HeadShots/Emilia.jpg" alt="Emilia's headshot" style="width:50%;height:50%;">
                 </div>
                 <div class="carousel-item">
                   <blockquote>
                     <p>"Great place quick service. Went to sell two rings cleaned and yearly inspected. While selling there we decided to sell a few odds and ends from around the house. The site offered high prices and was very easy to use. This company makes selling stuff a real pleasure. I love this company and the hospitality they give their customers."</p>
                     <footer>Tony Nguyen</footer>
                   </blockquote>
-                  <img src="../HeadShots/Tony.jpg" alt="Tony's headshot" style="width:60px;height:60px;">
+                  <img src="../HeadShots/Tony.jpg" alt="Tony's headshot" style="width:50%;height:50%;">
                 </div>
                 <div class="carousel-item">
                   <blockquote>
                     <p>"Always have stuff I can get rid of on here! Great pricing and friendly knowledgeable customer service! NEVER had issues doing business with them!!!"</p>
                     <footer>Rob Nickel</footer>
                   </blockquote>
-                  <img src="../HeadShots/Rob.jpg" alt="Rob's headshot" style="width:60px;height:60px;">
+                  <img src="../HeadShots/Rob.jpg" alt="Rob's headshot" style="width:50%;height:50%;">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -75,16 +75,16 @@
           </div>
         </div>
 
-        <div class= "col-md-6" style= "text-align: center; margin-top: 50px">
+        <div class= "col-md-6" style= "text-align: center;">
           <div class="container-fluid">
-            <div class="row" style="text-align: center; padding-left: 12%; padding-right: 12%; padding-top: 3%; padding-bottom: 10%;">
-                <img src="../assets/frontGraphic.png" alt="satisfaction" style="width:100%; height:80%; margin-right: 20px; margin-left: 20px;">
+            <div class="row" style="text-align: center; padding-left: 12%; padding-right: 12%; padding-top: 3%; padding-bottom: 15%;">
+                <img src="../assets/frontGraphic.png" alt="satisfaction" style="width:100%; height:80%;">
             </div>
           </div>
-          <img src="../assets/satisfaction.jpeg" alt="satisfaction" style="width:21%px;height:15%; margin-right: 20px; margin-left: 20px;">
-          <img src="../assets/reviews.png" alt="reviews" style="width:18%;height:15%; margin-right: 20px; margin-left: 20px;">
-          <img src="../assets/paidthemost.jpeg" alt="paid the most" style="width:18%;height:15%; margin-right: 20px; margin-left: 20px;">
-          <img src="../assets/bestprice.jpeg" alt="best price" style="width:15%;height:15%; margin-right: 20px; margin-left: 20px;">
+          <img src="../assets/satisfaction.jpeg" alt="satisfaction" style="width:21%;height:15%; margin-right: 2%; margin-left: 2%;">
+          <img src="../assets/reviews.png" alt="reviews" style="width:18%;height:15%; margin-right: 2%; margin-left: 2%;">
+          <img src="../assets/paidthemost.jpeg" alt="paid the most" style="width:18%;height:15%; margin-right: 2%; margin-left: 2%;">
+          <img src="../assets/bestprice.jpeg" alt="best price" style="width:15%;height:15%; margin-right: 2%; margin-left: 2%;">
           <br><br>
           <h1><b>What we buy</b></h1>
           <p> Get Rid of it serves as a great outlet to turn any of your old or unused items including jewelry, coins, electronics, gaming systems, phones, laptops, furniture,
@@ -93,7 +93,7 @@
         </div>
 
 
-        <div class="col-md-3" style="text-align: center; margin-top: 50px">
+        <div class="col-md-3" style="text-align: center;">
           <h1>Mission Statement</h1>
           <p> GetRidofIt is known for creating a simple buying concept<br> which offers the highest dollar cash payout for any<br>
              collectible items, furthermore, serving our customers in a<br> a manner that will generate a referral business that money can't buy.
