bdd4afa7a40c6b8acb95e86eaea81bc61122886a
Ian Farris <i676f971@ku.edu>
2018-11-15 19:04:32 -0600

    updated change password in settings page.

--- users/backEnd/changePassword.php
+++ users/backEnd/changePassword.php
@@ -16,6 +16,12 @@
     // Start user session
     session_start();
     // Variables
+
+    $stmt = $mysqli->prepare("SELECT * FROM Project_Users WHERE username= ? AND password = ? ;");
+    $stmt->bind_param("ss", $username, $old_hash);
+    $stmt2 = $mysqli->prepare("UPDATE Project_Users SET password= ? WHERE username= ? ;");
+    $stmt2->bind_param("ss", $new_hash, $username);
+
     $username =  $_SESSION['user_id'];
     $old1 = $_POST['old1'];
     $old2 =  $_POST['old2'];
@@ -23,14 +29,13 @@
     $new2 = $_POST['new2'];
 
     $old_hash = hash('sha512', $old1);
-    $correct_old_pass = mysqli_query($mysqli, "SELECT * FROM Project_Users WHERE username='$username' AND password='$old_hash';");
-
-
     $new_hash = hash('sha512', $new1);
-    $newPass = "UPDATE Project_Users SET password='$new_hash' WHERE username='$username';";
 
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($result);
 
-    if (mysqli_num_rows($correct_old_pass)){
+    if ($stmt->fetch()){
       //echo "Correct old password";
       if (($old1 == '') || ($old2 == '') || ($new1 == '') || ($new2 == '')){
         echo "Error: cannot have blank input for passwords";
@@ -41,8 +46,12 @@
       else if ($new1 != $new2){
         echo "Error: new password inputs do not match up";
       }
-      else if ($pass_result = $mysqli->query($newPass)){
+      else if(strlen($new1) < 8){
+        echo "new password must be 8 characters or longer.";
+      }
+      else if ($stmt2->execute()){
         echo "New password changed successfully";
+        header('Location: ../settings.php');
       }
     }
     else{
