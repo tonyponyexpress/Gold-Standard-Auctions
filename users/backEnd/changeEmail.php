04d0710e39cffbb726d55203bc8c189b35390115
Ian Farris <i676f971@ku.edu>
2018-11-15 19:38:16 -0600

    added prepared statement for change email.

--- users/backEnd/changeEmail.php
+++ users/backEnd/changeEmail.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
 * User settings: change Email
@@ -9,24 +10,27 @@
 *@package users
 *
 */
-
     // Access database
     include ('../../cms/sql_credentials.php');
     global $mysqli;
     // Start user session
     session_start();
     // Variables
+    $stmt = $mysqli->prepare("UPDATE Project_Users SET email= ? WHERE username= ? ;");
+    $stmt->bind_param("ss", $newEmail, $username);
+
     $username =  $_SESSION['user_id'];
     $newEmail =  $_POST['newEmail1'];
-
     $user = "UPDATE Project_Users SET email='$newEmail' WHERE username='$username';";
-    if ($user_result = $mysqli->query($user)) {
+
+    if ($stmt->execute()) {
         echo "Email updated succesfully";
+        header('Location: ../settings.php');
     }
     else {
         echo "Error";
     }
-
     // close connection
+    $stmt->close();
     $mysqli->close();
 ?>
