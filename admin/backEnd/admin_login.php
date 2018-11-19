06ea77af09215d74a308ce31ee61cd1f4fb36ef1
Ian Farris <i676f971@ku.edu>
2018-11-19 11:46:23 -0600

    changed query in admin login to use prepared statement.

--- admin/backEnd/admin_login.php
+++ admin/backEnd/admin_login.php
@@ -22,14 +22,16 @@
     // Global database variable
     global $mysqli;
 
+    $stmt = $mysqli->prepare("SELECT * FROM Project_Admins WHERE username= ? AND password= ?");
+    $stmt->bind_param("ss", $admin_user, $hashed);
     $admin_user = $_POST["username"];
     $admin_password = $_POST["password"];
     $hashed = hash('sha512', $admin_password); //hashing the admin password to check if this hashed password matches on in database
 
-    $result = mysqli_query($mysqli, "SELECT * FROM Project_Admins WHERE username='$admin_user' AND password='$hashed';");
+    //$result = mysqli_query($mysqli, "SELECT * FROM Project_Admins WHERE username='$admin_user' AND password='$hashed';");
 
     // Login credentials are valid
-    if (mysqli_num_rows($result)){
+    if ($stmt->execute()){
         // set session
         $_SESSION['admin_id'] = $admin_user;
         header('Location: ../admin_dashboard.php');
