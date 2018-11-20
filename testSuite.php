c5000bf6a3b8f0512e84dfd741e9b2a5ea023f20
tonyponyexpress <tonynguyen979@ku.edu>
2018-11-19 22:44:43 -0600

    adding in test suite

---
+++ testSuite.php
@@ -0,0 +1,23 @@
+<?php
+
+class TestSuite{
+
+  public function TestSuite(){
+    //empty constructor
+  }
+
+  /*Runs every single test*/
+  public function runTests(){
+
+  }
+
+  /*Test for creating users*/
+  private function test01(){
+
+  }
+
+
+
+}
+
+?>
