970e5e9e359eed667fc318ca49c0233712e941a4
tonyponyexpress <tonynguyen979@ku.edu>
2018-11-20 16:00:45 -0600

    saving  changes before pulling

--- testSuite.php
+++ testSuite.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+global $mysqli;
 
 class TestSuite{
 
@@ -8,13 +11,12 @@ class TestSuite{
 
   /*Runs every single test*/
   public function runTests(){
-    test01();
+
   }
 
-  /*Test for creating users*/
-  private function test01(){
+  /*Test for creating users with good input*/
+  public function test01(){
     include ('cms/sql_credentials.php');
-    global $mysqli;
 
     // Start session
     session_start();
@@ -28,51 +30,46 @@ class TestSuite{
 
     $query = "SELECT * FROM Project_Users WHERE username = '$username';";
     $query2 = "SELECT * FROM Project_Users WHERE email = '$email';";
-    $hashed = hash('sha512', $password); //hash the created user's password that will be stored in the database
+    $hashed = hash('sha512', $password1); //hash the created user's password that will be stored in the database
     $result = $mysqli->query($query);
     $result2 = $mysqli->query($query2);
 
-    echo "start";
-    
     if ($row = $result->fetch_assoc() || $row = $result2->fetch_assoc()){
-      echo "Test#01: Creating User FAILED<b>";
+      echo "Test#01: Creating User FAILED<br>";
     }
     else{
       $add_user = "INSERT INTO Project_Users (FirstName, LastName, username, email, password) VALUES ('$firstName','$lastName','$username','$email','$hashed');";
       if ($username == ''){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if ($email == ''){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if ($password1 == ''){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if ($password1 != $password2){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if ($firstName == ''){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if ($lastName == ''){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if (strlen($password1) < 8){
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
       else if ($user_result = $mysqli->query($add_user)){
-        echo "Test#01: Creating User PASSED<b>";
+        echo "Test#01: Creating User PASSED<br>";
       }
       else{
-        echo "Test#01: Creating User FAILED<b>";
+        echo "Test#01: Creating User FAILED<br>";
       }
     }
-    echo "end";
 
     $mysqli->close();
   }
-
-
 
 }
 
