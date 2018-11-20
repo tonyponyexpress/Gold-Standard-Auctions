289932cef56bb4bc42fb470e856f301812d229da
Emilia Paz <emilia.paz@ku.edu>
2018-11-19 23:59:16 -0600

    createUser test working

--- testButton.php
+++ testButton.php
@@ -1,5 +1,7 @@
 <?php
     include('testSuite.php');
     $homeScreen = new TestSuite();
-    $homeScreen->test01();
+    // createUsers
+    $homeScreen->createUsers("true","123","123456789","123456789","firstName","lastName","email@email.com");
+    $homeScreen->createUsers("true","","123456789","123456789","newww","newlast","new@email.com");
 ?>
--- testSuite.php
+++ testSuite.php
@@ -1,4 +1,7 @@
 <?php
+// error_reporting(E_ALL);
+// ini_set("display_errors",1);
+global $mysqli;
 
 class TestSuite{
 
@@ -8,69 +11,87 @@ class TestSuite{
 
   /*Runs every single test*/
   public function runTests(){
-    test01();
+
   }
 
   /*Test for creating users*/
-  private function test01(){
+  public function createUsers($test,$username,$password,$password2,$firstName,$lastName,$email){
 
     include ('cms/sql_credentials.php');
-    global $mysqli;
+
 
     // Start session
     session_start();
 
-    $username = "Test";
-    $password1 = "Thisiseight";
-    $password2 = "Thisiseight";
-    $firstName = "first";
-    $lastName = "last";
-    $email = "TestSuite@gmail.com";
+    //statements
+    $stmt2 = $mysqli->prepare("SELECT * FROM Project_Users WHERE username = ? ;");
+    $stmt2->bind_param("s", $username);
+    $stmt3 = $mysqli->prepare("SELECT * FROM Project_Users WHERE email = ? ;");
+    $stmt3->bind_param("s", $email);
+    $stmt = $mysqli->prepare("INSERT INTO Project_Users (FirstName, LastName, username, email, password) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $firstName, $lastName, $username, $email, $hashed);
 
-    $query = "SELECT * FROM Project_Users WHERE username = '$username';";
-    $query2 = "SELECT * FROM Project_Users WHERE email = '$email';";
+
+    $query = "SELECT * FROM Project_Users WHERE username = '$username'";
+    $query2 = "SELECT * FROM Project_Users WHERE email = '$email'";
     $hashed = hash('sha512', $password); //hash the created user's password that will be stored in the database
     $result = $mysqli->query($query);
     $result2 = $mysqli->query($query2);
 
-    echo "start";
+    $stmt3->execute();
+    $stmt3->store_result();
+    $stmt3->bind_result($result);
+    $result2 = $stmt2->execute();
 
-    if ($row = $result->fetch_assoc() || $row = $result2->fetch_assoc()){
-      echo "Test#01: Creating User FAILED<b>";
+
+    if ($stmt2->fetch()){
+        echo "Username already exists";
+    }
+    elseif ($stmt3->fetch()){
+        echo "Email already exists";
     }
     else{
-      $add_user = "INSERT INTO Project_Users (FirstName, LastName, username, email, password) VALUES ('$firstName','$lastName','$username','$email','$hashed');";
-      if ($username == ''){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if ($email == ''){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if ($password1 == ''){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if ($password1 != $password2){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if ($firstName == ''){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if ($lastName == ''){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if (strlen($password1) < 8){
-        echo "Test#01: Creating User FAILED<b>";
-      }
-      else if ($user_result = $mysqli->query($add_user)){
-        echo "Test#01: Creating User PASSED<b>";
-      }
-      else{
-        echo "Test#01: Creating User FAILED<b>";
-      }
-    }
-    echo "end";
 
+        if ($username == ''){
+            echo "Error: cannot have blank username <br>";
+        }
+        else if ($email == ''){
+            echo "Error: cannot have blank email <br>";
+        }
+        else if ($password == ''){
+            echo "Error: cannot have blank password <br>";
+        }
+        else if ($password != $password2){
+            echo "Error: Passwords do not match up <br>";
+        }
+        else if ($firstName == ''){
+            echo "Error: cannot have blank first name <br>";
+        }
+        else if ($lastName == ''){
+            echo "Error: cannot have blank last name <br>";
+        }
+        else if (strlen($password) < 8){
+            echo "Error: password must be 8 characters or longer <br>";
+        }
+        else if($stmt->execute()){
+            echo "New user created successfully <br>";
+        }
+        else{
+            echo "Error: " . $mysqli->error;
+        }
+        $_SESSION['error'] = $error;
+        if($test="false"){
+            header('Location: ../homeScreen.php');
+        }
+
+    }
+
+    $stmt2 ->close();
+    $stmt3->close();
+    $stmt ->close();
     $mysqli->close();
+
+
   }
 
 
